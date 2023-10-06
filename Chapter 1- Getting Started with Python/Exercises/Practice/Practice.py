86ea7e630832d7d4141aecd48594ef33c9058560
Yyan <144330036+YyanQF@users.noreply.github.com>
2023-10-07 00:39:40 +0400

    Practice.py

--- Chapter 1- Getting Started with Python/Exercises/Practice/Practice.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice/Practice.py
@@ -1,13 +1,23 @@
 name = input ("Enter Name: ")
 print (name)
-'''name = input ("Enter Name: ")
-print (name)'''
+'''name = input ("Enter Name: ")'''
 age = input ("Enter your age: ")
 print (age)
-'''age = input ("Enter your age: ")
-print (age)'''
+'''age = input ("Enter your age: ")'''
+your_number = input ("Enter your mobile number: ")
+print (your_number)
+'''your_number = input ("Enter your mobile number: ")'''
+address = input ("Enter your address: ")
+print (address)
+'''address = input ("Enter your address: ")'''
 father_name = input ("Enter your father's name: ")
 print (father_name)
-'''father_name = input ("Enter your father's name: ")
-print (father_name)'''
-print ("meow"*10000)
+'''father_name = input ("Enter your father's name: ")'''
+fathers_number = input ("Enter your father's number: ")
+print (fathers_number)
+'''fathers_number = input ("Enter your father's number: ")'''
+mother_name = input ("Enter your mother's name: ")
+print (mother_name)
+'''mother_name = input ("Enter your mother's name: ")'''
+mothers_number = input ("Enter your mother's number: ")
+print (mothers_number)
