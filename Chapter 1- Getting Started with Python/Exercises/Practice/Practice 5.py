dcba58fdd8633773114b69aa353bfb1266654c03
Yyan <144330036+YyanQF@users.noreply.github.com>
2023-10-12 01:05:20 +0400

    Practice 5

--- Chapter 1- Getting Started with Python/Exercises/Practice/Practice 5.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice/Practice 5.py
@@ -1,1 +1,8 @@
-#
+#Write four strings in different variables and print the output 
+
+str1 = "Gingy the "
+str2 = "Cat "
+str3 = "meows at "
+str4 = "night loudy"
+
+print (str1 + str2 + str3 + str4)
