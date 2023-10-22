42261e49067ebf511979ec47787d9f2047097b31
Yyan <144330036+YyanQF@users.noreply.github.com>
2023-10-23 00:02:11 +0400

    Chapter3Exercise4.py

---
+++ Chapter 3- Data Structures/Exercise4.py
@@ -0,0 +1,7 @@
+guests = ['Gingy', 'Faus', 'Emmy', 'Feral', 'Dunc']
+names = guests
+print(f"Greetings, {names[0]} meow meow")
+print(f"Greetings, {names[1]} I would like to invite you to a dinner party I am hosting!")
+print(f"Greetings, {names[2]} I would like to invite you to a dinner party I am hosting!")
+print(f"Greetings, {names[3]} I would like to invite you to a dinner party I am hosting!")
+print(f"Greetings, {names[4]} I would like to invite you to a dinner party I am hosting!")
