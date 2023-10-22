5a69d8ba4f3cb2b08a83539b16f2f532bc967e5c
Yyan <144330036+YyanQF@users.noreply.github.com>
2023-10-22 23:48:26 +0400

    Chapter3Exercise2.py

---
+++ Chapter 3- Data Structures/Exercise2.py
@@ -0,0 +1,14 @@
+names = ['Gingy', 'Isaac', 'Gab', 'Rafael', 'Wayeth', 'John']
+
+message = f"Meow Meow!, {names[0]} meow meow"
+print(message)
+message = f"Hello, {names[1]} how have you been?"
+print(message)
+message = f"Hello, {names[2]} how have you been?"
+print(message)
+message = f"Hello, {names[3]} how have you been?"
+print(message)
+message = f"Hello, {names[4]} how have you been?"
+print(message)
+message = f"Hello, {names[5]} how have you been?"
+print(message)
