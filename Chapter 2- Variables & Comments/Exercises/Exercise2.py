638a53c950b4bdaa0309e45fad7f13dd641415ab
Yyan <144330036+YyanQF@users.noreply.github.com>
2023-10-22 23:47:23 +0400

    Chapter2Exercise2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -0,0 +1,4 @@
+quote_from = ("Koro Sensei from a famous manga series of Shonen Jump once said,")
+quote = ('"To a young person for whom everything comes easily, the pain of defeat can be a much needed wake up call."')
+
+print (quote_from, quote)
