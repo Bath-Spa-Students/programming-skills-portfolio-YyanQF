d9f71f71aefeaa0b8d368474e3fedfe0212b2809
Yyan <144330036+YyanQF@users.noreply.github.com>
2023-10-23 22:38:55 +0400

    Chapter2Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,13 @@
+Cat_Name = "\tGingy the Cat\n"
+
+print("Unmodified:")
+print(Cat_Name)
+
+print("\nUsing lstrip():")
+print(Cat_Name.lstrip())
+
+print("\nUsing rstrip():")
+print(Cat_Name.rstrip())
+
+print("\nUsing strip():")
+print(Cat_Name.strip())
