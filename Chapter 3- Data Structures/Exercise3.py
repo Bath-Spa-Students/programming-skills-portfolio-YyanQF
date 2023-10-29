eb3caa6b0951c0f28cd6fd8da31c6f2ef7a711bd
Yyan <144330036+YyanQF@users.noreply.github.com>
2023-10-29 19:13:30 +0400

    Chapter3Exercise3.py

--- Chapter 3- Data Structures/Exercise3.py
+++ Chapter 3- Data Structures/Exercise3.py
@@ -5,3 +5,12 @@ print(f"Greetings, {names[1]} I would like to invite you to a dinner party I am 
 print(f"Greetings, {names[2]} I would like to invite you to a dinner party I am hosting!")
 print(f"Greetings, {names[3]} I would like to invite you to a dinner party I am hosting!")
 print(f"Greetings, {names[4]} I would like to invite you to a dinner party I am hosting!")
+Mode_Of_Transportation = ('Hyundai Santa Fe', 'Suzuki Hayabusa', 'Enve Melee')
+message = f"I would like to own a, {Mode_Of_Transportation[0]} when I get older and use it with my family"
+print(message)
+
+message = f"I would like to own a, {Mode_Of_Transportation[1]} and ride along highways"
+print(message)
+
+message = f"I would like to own a, {Mode_Of_Transportation[2]} and cycle around the park"
+print(message)
