e2e9725e6d9ce2a82ce4312f5355dd4fe0076bd5
Yyan <144330036+YyanQF@users.noreply.github.com>
2023-11-01 17:09:58 +0400

    Chapter4Exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,32 @@
+#If the alien is green, print a message that the player earned 5 points.
+alien_color = 'green'
+print("Player you hit the Alien! Alien is Green")
+
+if alien_color == 'green':
+    print("Player you have earned 5 points!")
+elif alien_color == 'yellow':
+    print("Player you have earned 10 points!")
+else:
+    print("Player you have earned 15 points!")
+
+#If the alien is yellow, print a message that the player earned 10 points.
+alien_color = 'yellow'
+print("\nPlayer you hit the Alien! Alien is Yellow")
+
+if alien_color == 'green':
+    print("Player you have earned 5 points!")
+elif alien_color == 'yellow':
+    print("Player you have earned 10 points!")
+else:
+    print("Player you have earned 15 points!")
+
+# If the alien is red, print a message that the player earned 15 points.
+alien_color = 'red'
+print("\nPlayer you hit the Alien! Alien is Red")
+
+if alien_color == 'green':
+    print("Player you have earned 5 points!")
+elif alien_color == 'yellow':
+    print("Player you have earned 10 points!")
+else:
+    print("Player you have earned 15 points!")
